3c456fa52518355d5a2624a31fc52a62daa188f4
Waite0603 <waite0603@qq.com>
2025-03-03 11:31:22 +0800

    🐞 fix: 订正管道整体不移动问题

--- flappy_bird/assets/Scripts/Bird.ts
+++ flappy_bird/assets/Scripts/Bird.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Input, input, Node, RigidBody2D, Vec2 } from 'cc';
+import { _decorator, Collider2D, Component, Contact2DType, Input, input, IPhysics2DContact, Node, RigidBody2D, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Bird')
@@ -7,11 +7,11 @@ export class Bird extends Component {
     // 监听游戏是否开始
     private _canControl: boolean = true;
 
-    private rgd2D:RigidBody2D = null;
+    private rgd2D: RigidBody2D = null;
 
     // 小鸟旋转速度
     @property
-    rotateSpeed:number = 30;
+    rotateSpeed: number = 60;
 
     protected start(): void {
         this.rgd2D = this.getComponent(RigidBody2D);
@@ -19,6 +19,14 @@ export class Bird extends Component {
 
     onLoad() {
         input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+
+        // 注册单个碰撞体的回调函数
+        let collider = this.getComponent(Collider2D);
+        if (collider) {
+            collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+        }
+
     }
 
     onDestroy() {
@@ -31,13 +39,24 @@ export class Bird extends Component {
         console.log('onTouchStart');
         this.rgd2D.linearVelocity = new Vec2(0, 10);
         // this.node.setRotationFromEuler(0, 0, 30);
-        this.node.angle = 30; 
+        this.node.angle = 30;
     }
 
 
+    onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        console.log(otherCollider.tag);
+    }
+    onEndContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        // 只在两个碰撞体结束接触时被调用一次
+        console.log('onEndContact');
+    }
 
     update(deltaTime: number) {
         this.node.angle -= this.rotateSpeed * deltaTime;
+
+        if (this.node.angle < -60) {
+            this.node.angle = -60;
+        }
 
     }
 }
